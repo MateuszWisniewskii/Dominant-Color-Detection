b55283f300a1256034738d32bd1a6addc201d44d
Patryk Pandel <patrykpandel@MacBook-Pro-Patryk.local>
2025-04-16 22:07:57 +0200

    test

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-file_path = "../test_files/obrazek1.png"
+import numpy as np
+from PIL import Image
 
-#file_path = input("Podaj nazwę pliku wraz z rozszerzeniem i ścieżką: ")
 
-try:
-    file_handle = open(file_path, "rb")
-    print("Udało się otworzyć plik :)")
-except:
-    print(f"Nie odnaleziono pliku: {file_path}")
-    print("Sprawdź jeszcze raz ścieżkę do pliku i uruchom program na nowo") 
-
-for line in file_handle:
-    print(line)
-
-print("=======================================================")
+print("hello")
 
