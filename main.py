2974076a202b6559457d3f2e789bca025778c2bf
MateuszWisniewskii <mdw180703@gmail.com>
2025-04-23 20:09:12 +0200

    lil update

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import numpy as np
-from PIL import Image
+#from PIL import Image
 from imageLoader import ImageLoader
 
+# short for separator
+def sep():
+    print("=============================================================")
 
-loader = ImageLoader()
+images_folder = "images_from_teams/"
 
-loader.open("Data/")
+# magic number by now, 'couse images in /images_fro_teams are in many different sizes, some of tehem are jutr taller than wider
+image_X = 300
+image_Y = 300
+image_size = (image_X, image_Y) 
+
+input_layer_nerurons_amount = image_X * image_Y
+output_layer_nerurons_amount = 3 # Only three because we are going to ingnore alpha factor
+
+neurons_in_layer = [input_layer_nerurons_amount, output_layer_nerurons_amount]
+
+
+loader = ImageLoader(image_size)
+
+loader.open(images_folder)
+
+
+
+sep()
